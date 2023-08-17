b850938a3ef9d7e079b0210250f6655615ce27e3
Arch-Noize <santiagonoize@gmail.com>
2023-08-17 08:45:41 -0400

    Add function events

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './styles.css';
 import PropTypes from 'prop-types';
 
-function Button(props) {
-  const { number } = props;
+const Button = (props) => {
+  const { number, handleEvent } = props;
   if (number === '\u00F7' || number === 'x' || number === '-' || number === '+' || number === '=') {
-    return <button type="button" className="button" id="operator">{number}</button>;
+    return <button type="button" className="button" id="operator" onClick={handleEvent}>{number}</button>;
   } if (number === '0') {
-    return <button type="button" className="button" id="zero">{number}</button>;
+    return <button type="button" className="button" id="zero" onClick={handleEvent}>{number}</button>;
   }
-  return <button type="button" className="button">{number}</button>;
-}
+  return <button type="button" className="button" onClick={handleEvent}>{number}</button>;
+};
 
 Button.propTypes = {
   number: PropTypes.number.isRequired,
+  handleEvent: PropTypes.func.isRequired,
 };
 
 export default Button;
