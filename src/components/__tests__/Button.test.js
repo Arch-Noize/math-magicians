2a43e9f810ab4269b3081182e74068cf86dce561
Arch-Noize <santiagonoize@gmail.com>
2023-08-24 08:18:11 -0400

    Add different test Button

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Button from '../Button';
+import { waitFor } from '@testing-library/react';
 
 it('renders correctly', () => {
     const handleEvent = jest.fn();
-    const {tree} = render(<Button number={1} handleEvent={handleEvent}/>)
-
-    expect(tree).toMatchSnapshot();
+    //const {tree} = render(<Button number={1} handleEvent={handleEvent}/>)
+    const tree = renderer.create(<Button number={1} handleEvent={handleEvent}/>).toJSON;
+    expect(tree).toMatchSnapshot();   
 });
