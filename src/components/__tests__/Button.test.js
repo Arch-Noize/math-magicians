5bd1878ecc260721eaa94ece80b0236b70436eae
Arch-Noize <santiagonoize@gmail.com>
2023-08-25 06:18:01 -0400

    Add interaction test

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render , fireEvent, getByText} from '@testing-library/react';
 import Button from '../Button';
 
 describe('Loading button', () => {
+  const handleEvent = jest.fn();
+  
   it('renders correctly', () => {
-    const handleEvent = jest.fn();
     const { tree } = render(<Button number={1} handleEvent={handleEvent} />);
     expect(tree).toMatchSnapshot();
   });
+
+  it('Test if button is pressed', () => {
+    const { getByText } = render(<Button number={9} handleEvent={handleEvent} />);
+    const button = getByText(9);
+
+    fireEvent.click(button);
+    expect(handleEvent).toHaveBeenCalled();
+  })  
 });
--- src/components/__tests__/Navbar.test.js
+++ src/components/__tests__/Navbar.test.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render , screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
 import Navbar from '../Navbar';
 
 describe('Loading calculator component', () => {
-  it('renders correctly', () => {
-    const { tree } = render(
+
+  it('Renders correctly', () => {
+    const { } = render(
       <BrowserRouter>
         <Navbar />
       </BrowserRouter>,
     );
-    expect(tree).toMatchSnapshot();
+    
+    const title = screen.getByText('Math Magicians')
+    expect(title).toBeInTheDocument();
   });
+
+  it('Renders correct links', () => {
+    const { } = render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    );
+
+    const home = screen.getByRole('link' , { name: 'Home' });
+    const calc = screen.getByRole('link' , { name: 'Calculator' })
+    const quote = screen.getByRole('link' , { name: 'Quote' })
+
+    expect(home).toHaveAttribute('href' , '/')
+    expect(calc).toHaveAttribute('href' , '/calculator')
+    expect(quote).toHaveAttribute('href' , '/quote')
+  })
 });
